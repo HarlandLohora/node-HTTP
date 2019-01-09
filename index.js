76481c069b508d68e3296a13b29c1155c033138a
HarlandLohora <lohoraharland@gmail.com>
2019-01-09 12:07:12 -0600

    Routes

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const http = require('http');
 
 const serve = http.createServer(( req, res )=>{
-  res.writeHead(404,{ 'content-type': 'text/plain'});
-  res.write('<h1>Harland Lohora</h1>');
-  res.end();
+  console.log(req.url);
+  if( req.url === '/'){
+    res.writeHead(200,{ 'content-type': 'html'});
+    res.write('<h1>Harland Lohora - Home</h1>');
+    res.end();
+  }else if( req.url === '/contact'){
+    res.writeHead(200,{ 'content-type': 'html'});
+    res.write('<h1>Contact</h1>');
+    res.end();
+  }else{
+    res.writeHead(404,{ 'content-type': 'html'});
+    res.write('<h1>404 Not Found!</h1>');
+    res.end();
+  }
+
 });
 
-serve.listen(3000);
+serve.listen(3000, () => {
+  console.log('Successfully connection!');
+});
